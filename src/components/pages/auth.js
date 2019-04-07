49ce3ef631d29207bd2bad437bf6242334f63086
Owner <owner@Users-MacBook-Pro.local>
2019-04-07 17:43:52 -0400

    created the auth component. Created grid for auth page. Imported image onto page

---
+++ src/components/pages/auth.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+import loginImage from '../../../static/assets/images/auth/login.jpg'
+
+export default class Auth extends Component {
+  render() {
+    return (
+      <div className="auth-page-wrapper">
+        <div 
+            className="left-column"
+            style = {{
+                backgroundImage: `url(${loginImage})`
+            }}
+         />
+
+        <div className="right-column">
+          <h1> Login Component goes here..</h1>
+        </div>
+      </div>
+    );
+  }
+}
